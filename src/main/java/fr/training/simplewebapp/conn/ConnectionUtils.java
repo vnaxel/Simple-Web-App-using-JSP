947e7d21abb740152528da555efda050c986924b
Axel Yvenou <axel.yvenou@gmail.com>
2021-09-29 23:30:13 +0200

    created connection classes, credentials for DB are in the MySQLConnUtils which is ignored

---
+++ src/main/java/fr/training/simplewebapp/conn/ConnectionUtils.java
@@ -0,0 +1,27 @@
+package fr.training.simplewebapp.conn;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionUtils {
+	
+	public static Connection getConnection()
+	throws ClassNotFoundException, SQLException {
+		return MySQLConnUtils.getMySQLConnection();
+	}
+	
+	public static void closequietly(Connection conn) {
+		try {
+			conn.close();
+		} catch (Exception e) {
+		}
+	}
+	
+	public static void rollbackQuietly(Connection conn) {
+		try {
+			conn.rollback();
+		} catch (Exception e) {
+		}
+	}
+
+}
